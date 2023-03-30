5525b194ed4ed8ac7479ac39790e0b5407542e27
baldman110 <129371796+baldman110@users.noreply.github.com>
2023-03-30 16:23:03 +0800

    Add files via upload

---
+++ 33 (2).py
@@ -0,0 +1,16 @@
+def isprime(n):
+    for j in range(2, n // 2):
+        if n % j == 0:
+            return 0
+    return n
+
+n=int(input())
+for i in range(2,1000):
+    x=isprime(i)
+    if x!=0:
+        while n%x==0:
+            print(x,end=" ")
+            n=n//x
+            if n==0:
+                break
+
